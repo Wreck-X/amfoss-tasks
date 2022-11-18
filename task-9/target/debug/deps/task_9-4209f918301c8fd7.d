c5eaf9d174c408ee10cc8c4820477a401d55d246
Wreck-X <hemanthkrishnakumar@gmail.com>
2022-11-18 15:47:57 +0530

    fixed csv

--- task-9/target/debug/deps/task_9-4209f918301c8fd7.d
+++ task-9/target/debug/deps/task_9-4209f918301c8fd7.d
@@ -1,5 +1,5 @@
-/home/wreck/Desktop/Projects/task_9/target/debug/deps/task_9-4209f918301c8fd7.rmeta: src/main.rs
+/home/wreck/Desktop/Projects/task-9/target/debug/deps/task_9-4209f918301c8fd7.rmeta: src/main.rs
 
-/home/wreck/Desktop/Projects/task_9/target/debug/deps/task_9-4209f918301c8fd7.d: src/main.rs
+/home/wreck/Desktop/Projects/task-9/target/debug/deps/task_9-4209f918301c8fd7.d: src/main.rs
 
 src/main.rs:
--- task-9/target/debug/deps/task_9-4c7874d4fef4c24c.d
+++ task-9/target/debug/deps/task_9-4c7874d4fef4c24c.d
@@ -1,5 +1,5 @@
-/home/wreck/Desktop/Projects/task_9/target/debug/deps/task_9-4c7874d4fef4c24c: src/main.rs
+/home/wreck/Desktop/Projects/task-9/target/debug/deps/task_9-4c7874d4fef4c24c: src/main.rs
 
-/home/wreck/Desktop/Projects/task_9/target/debug/deps/task_9-4c7874d4fef4c24c.d: src/main.rs
+/home/wreck/Desktop/Projects/task-9/target/debug/deps/task_9-4c7874d4fef4c24c.d: src/main.rs
 
 src/main.rs:
