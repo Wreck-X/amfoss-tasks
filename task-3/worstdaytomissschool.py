e822ac96ffd22092604cc86659fbe823ee14a67e
Wreck-X <hemanthkrishnakumar@gmail.com>
2022-11-18 15:40:30 +0530

    task 3 progressed

--- task-3/worstdaytomissschool.py
+++ task-3/worstdaytomissschool.py
@@ -1,49 +1,97 @@
-n = int(input(""))
-a = list(map(int,input("").split()))
-majlist = []
-for i in a:
-    if i == 4:
-        majlist.append([a.pop(a.index(i))])
-for j in a:
-    ap1 = []
-    if j == 1:
-        ap1.append(a.pop(a.index(j)))
-        for k in a:
-            if k == 3:
-                ap1.append(a.pop(a.index(k)))
-                break
-    if ap1!= []:
-        majlist.append(ap1)
+inp = input()
+lis = [*map(int,input().split())]
+finalcount = 0
 
-for k in a:
-    ap2= []
-    if k == 3:
-        ap2.append(a.pop(a.index(k)))
-        for l in a:
-            if l == 1:
-                ap2.append(a.pop(a.index(l)))
-                break
-    if ap2!= []:
-        majlist.append(ap2)
+count4 = lis.count(4)
+finalcount = finalcount + count4
+for i in range(count4):
+    lis.remove(4)
 
-for m in a:
-    ap3 = []
-    if m == 2:
-        ap3.append(a.pop(a.index(m)))
-        for n in a:
-            if n == 2:
-                ap3.append(a.pop(a.index(n)))
-                break
-    if ap3 != []:
-        majlist.append(ap3)
+count3 = lis.count(3)
+count1 = lis.count(1)
+if count3 > count1:
+    count3 = count3 - count1
+    finalcount = finalcount + count1
+    for i in range(count1):
+        lis.remove(3)
+        lis.remove(1)
+    count1 = 0
+    oneinlist = False
+    threeinlist = True
 
-count1 = a.count(1)
-numcar1 = int(count1/4)
-rem = count1%4
-for i in range(numcar1):
-    majlist.append([1,1,1,1])
-    for i in range(4):
-        a.pop(1)
-if a != []:
-    majlist.append(a)
-print(len(majlist))          
+if count1 > count3:
+    count1 = count1 - count3
+    finalcount = finalcount + count3
+    for i in range(count3):
+        lis.remove(1)
+        lis.remove(3)
+    count3 = 0
+    oneinlist = True
+    threeinlist = False
+
+if count1 == count3:
+    finalcount = finalcount + count1
+    for i in range(count1):
+        lis.remove(1)
+        lis.remove(3)
+    count1 = 0
+    count3 = 0
+    oneinlist = False
+    threeinlist = False
+
+count2 = lis.count(2)
+ap2 = count2//2
+ap2rem = count2%2
+
+for i in range(ap2*2):
+    lis.remove(2)
+
+finalcount = finalcount+ap2
+
+count1 = lis.count(1)
+count2 = lis.count(2)
+dev1 = int(count1/2)
+rem1 = count1%2
+count1 = lis.count(1)
+count2_ = lis.count(2) * 2
+count2 = lis.count(2)
+
+count1 = lis.count(1)
+count1__ = lis.count(1)//4
+count1_ = count1 // 4 * 4
+
+finalcount = finalcount + count1__
+if count1 > 4:
+    for i in range(count1_):
+        lis.remove(1)
+
+if lis == [1]:
+    finalcount = finalcount + 1
+if lis == [1,1]:
+    finalcount = finalcount + 1
+if lis == [1,1,1]:
+    finalcount = finalcount + 1
+if lis == [2]:
+    finalcount = finalcount + 1
+if lis == [3]:
+    finalcount = finalcount + 1
+if lis == [2,1,1] or lis == [1,2,1] or lis == [1,1,2]:
+    finalcount = finalcount + 1
+if lis == [2,1,1,1] or lis == [1,2,1,1] or lis == [1,1,2,1] or lis == [1,1,1,2]:
+    finalcount = finalcount + 2
+if lis == [1,2] or lis == [2,1]:
+    finalcount = finalcount + 1
+
+if len(set(lis)) == 1 and set(lis) == {3} and len(lis) > 1:
+    for i in lis:
+        finalcount = finalcount + 1
+
+count3 = lis.count(3)
+count2 = lis.count(2)
+countt = count3 + count2
+
+if count2%2 != 0:
+    if len(lis) == countt:
+        finalcount = finalcount + len(lis)
+
+print(finalcount)
--- test.py
+++ test.py
@@ -1,37 +1,97 @@
-import math
-input = [*map(int,input().split())]
-num1 = input[0]
-num2 = input[1]
+inp = input()
+lis = [*map(int,input().split())]
+finalcount = 0
 
-list_ = []
-divid = num2/num1
-remainder = num2%num1
+count4 = lis.count(4)
+finalcount = finalcount + count4
+for i in range(count4):
+    lis.remove(4)
 
-if remainder != 0:
-    print(-1)
-    exit()
-else: 
-    while divid % 2 == 0:
-        list_.append(2)
-        divid = divid /2
-    for i in range(3,int(math.sqrt(divid))+1,2):
-        while divid%i==0:
-            list_.append(i)
-            divid = divid/i
-    if divid > 2:
-        list_.append(divid)
+count3 = lis.count(3)
+count1 = lis.count(1)
+if count3 > count1:
+    count3 = count3 - count1
+    finalcount = finalcount + count1
+    for i in range(count1):
+        lis.remove(3)
+        lis.remove(1)
+    count1 = 0
+    oneinlist = False
+    threeinlist = True
 
-count2 = 0
-count3 = 0
+if count1 > count3:
+    count1 = count1 - count3
+    finalcount = finalcount + count3
+    for i in range(count3):
+        lis.remove(1)
+        lis.remove(3)
+    count3 = 0
+    oneinlist = True
+    threeinlist = False
 
-for i in list_:
-    if i not in [2,3]:
-        print(-1)
-        exit()
+if count1 == count3:
+    finalcount = finalcount + count1
+    for i in range(count1):
+        lis.remove(1)
+        lis.remove(3)
+    count1 = 0
+    count3 = 0
+    oneinlist = False
+    threeinlist = False
 
-for i in list_:
-    if i == 2:
-        count2 = count2 + 1
-    if i == 3:
-        count3 = count3 + 1
-print(count2 + count3)
+count2 = lis.count(2)
+ap2 = count2//2
+ap2rem = count2%2
+
+for i in range(ap2*2):
+    lis.remove(2)
+
+finalcount = finalcount+ap2
+
+count1 = lis.count(1)
+count2 = lis.count(2)
+dev1 = int(count1/2)
+rem1 = count1%2
+count1 = lis.count(1)
+count2_ = lis.count(2) * 2
+count2 = lis.count(2)
+
+count1 = lis.count(1)
+count1__ = lis.count(1)//4
+count1_ = count1 // 4 * 4
+
+finalcount = finalcount + count1__
+if count1 > 4:
+    for i in range(count1_):
+        lis.remove(1)
+
+if lis == [1]:
+    finalcount = finalcount + 1
+if lis == [1,1]:
+    finalcount = finalcount + 1
+if lis == [1,1,1]:
+    finalcount = finalcount + 1
+if lis == [2]:
+    finalcount = finalcount + 1
+if lis == [3]:
+    finalcount = finalcount + 1
+if lis == [2,1,1] or lis == [1,2,1] or lis == [1,1,2]:
+    finalcount = finalcount + 1
+if lis == [2,1,1,1] or lis == [1,2,1,1] or lis == [1,1,2,1] or lis == [1,1,1,2]:
+    finalcount = finalcount + 2
+if lis == [1,2] or lis == [2,1]:
+    finalcount = finalcount + 1
+
+if len(set(lis)) == 1 and set(lis) == {3} and len(lis) > 1:
+    for i in lis:
+        finalcount = finalcount + 1
+
+count3 = lis.count(3)
+count2 = lis.count(2)
+countt = count3 + count2
+print(lis)
+if count2%2 != 0:
+    if len(lis) == countt:
+        finalcount = finalcount + len(lis)
+print(lis)
+print(finalcount)
