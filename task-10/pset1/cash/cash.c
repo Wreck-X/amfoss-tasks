d6235b56d278aed5e21ff861a7eead0b5cf814bf
Wreck-X <hemanthkrishnakumar@gmail.com>
2022-12-07 00:24:35 +0530

    task10 progress

--- task-10/pset1/cash/cash.c
+++ task-10/pset1/cash/cash.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
-int main(void) {
-int count = 0;
-int cash = round(get_float("enter cash- ")*100);
-while(cash!= 0){
-    if(cash>=25){
-        cash = cash - 25;
-        count++;
 
-    }
-    else if(cash>=10){
-        cash = cash - 10;
-        count++;
-    }
-    else if(cash>=5){
-        cash = cash - 5;
-        count++;
-    }
-    else if(cash>=1){
-        cash = cash - 1;
-        count++;
-    }
+
+int main(void)
+{
+
+int count = 0;
+
+while(true)
+{
+    float cash = get_float("enter cash- ");
+if (cash < 1){
+    cash = round(cash*100);
 }
-printf("%i",count);
+
+    if (cash > 0)
+    {
+
+        while(cash!= 0)
+        {
+            if(cash>=25){
+                cash = cash - 25;
+                count++;
+                continue;
+            }
+            else if(cash>=10){
+                cash = cash - 10;
+                count++;
+                continue;
+            }
+            else if(cash>=5){
+                cash = cash - 5;
+                count++;
+                continue;
+            }
+            else if(cash>=1){
+                cash = cash - 1;
+                count++;
+                continue;
+            }
+        }
+    }
+
+    else
+    {
+        continue;
+    }
+    break;
+}
+printf("%i\n",count);
 }
--- task-6/flutter_application_1/lib/main.dart
+++ task-6/flutter_application_1/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/components.dart';
-import 'dart:ui';
 import 'directions.dart';
 import 'package:flutter/services.dart';
 
