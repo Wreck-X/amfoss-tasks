18c72153e7bfc1f68a47c4c216ba52dc88d7f821
Wreck-X <hemanthkrishnakumar@gmail.com>
2022-12-13 17:50:45 +0530

    more final changes :/

--- task-10/pset2/ceasar.c
+++ task-10/pset2/ceasar.c
@@ -1,23 +1,70 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main (int argc, string argv[]){
-    if (argc > 1){
-        int loop;
+int encipher(string plaintext, int key);
+int test_arguments(int arguments, string key[]);
+
+int main(int argc, string argv[])
+{
+    int test = test_arguments(argc, argv);
+    if (test)
+    {
+        return test;
+    }
+    else
+    {
+        string plaintext = get_string("plaintext: ");
         int key = atoi(argv[1]);
-        string string_ = get_string("enter string- ");
-        int length = strlen(string_);
-        int count;
-        for (count = 0;count < length;count++){
-            char letter = string_[count] + key;
-            printf("%c",letter);
+        return encipher(plaintext, key);
+    }
+}
+
+int test_arguments(int arguments, string key[])
+{
+    if (arguments != 2)
+    {
+        printf("Usage: ./substitution key.\n");
+        return 1;
+    }
+    for (int i = 0, n = strlen(key[1]); i < n; i++)
+    {
+        if (isalpha(key[1][i]))
+        {
+            printf("Usage: ./caesar key.\n");
+            return 1;
         }
     }
-    else{
-        printf("Fail");
+    return 0;
+}
+
+int encipher(string plaintext, int key)
+{
+    printf("ciphertext: ");
+    char ci;
+    int n = strlen(plaintext);
+    char ciphertext[n];
+    for (int i = 0; i < n; i++)
+    {
+        int c = plaintext[i];
+
+        if (isalpha(c))
+        {
+            ci = c + key % 26;
+            bool test_boundaries = islower(ci) || isupper(ci);
+            if (!test_boundaries)
+            {
+                ci -= 26;
+            }
+        }
+        else
+        {
+            ci = c;
+        }
+        ciphertext[i] = ci;
     }
-    
+    printf("%s\n", ciphertext);
+    return 0;
 }
